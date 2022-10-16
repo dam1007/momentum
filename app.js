2d7701e7882ef93d23f133fd4543e0c4808c2f5c
dam1007 <dam1007@naver.com>
2022-10-16 23:32:54 +0900

    1. 폼요소 만들기 완성

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
 
-function onLoginSubmit() {
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
+
+//1. submit 클릭 시 로컬 스토리지에 유저정보 저장하기
+function onLoginSubmit(event) {
+    event.preventDefault(); //submit 기본 이벤트 사용 금지
     const username = loginInput.value;
-    console.log(username);
-
-    //JS 이용한 유효성 검사
-    /* if( username === '' ) {
-        alert('Please write your name.');
-    } else if ( username.length > 15) {
-        alert('Your name is too long.');
-    } */
-
-    //브라우저 이용한 유효성 검사
+    localStorage.setItem(USERNAME_KEY, username); //로컬 스토리지에 유저명 저장.
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    paingGreetings(username);
 }
 
-loginButton.addEventListener('click', onLoginSubmit);
+//3. 함수화
+function paingGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+//2. 로컬 스토리지에 유저정보 유무 확인하여 조건 처리하기
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // 정보 없으면 show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+    // 있으면 show the greetings
+    paingGreetings(savedUsername);
+}
